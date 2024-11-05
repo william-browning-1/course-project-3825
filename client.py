0872577f4e6ba40c33c22177f9d1ba1dc2b370c3
Will Browning <wdbrwnng@memphis.edu>
2024-11-05 12:19:34 -0600

    Cross Compatibility

--- client.py
+++ client.py
@@ -1,40 +1,62 @@
-# Python program to implement client side of chat room. 
-import socket 
-import select 
-import sys 
+import socket
+import select
+import sys
+import os
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-if len(sys.argv) != 3: 
-	print ("Correct usage: script, IP address, port number")
-	exit() 
-IP_address = str(sys.argv[1]) 
-Port = int(sys.argv[2]) 
-server.connect((IP_address, Port)) 
+def main():
+    # Check command-line arguments
+    if len(sys.argv) != 3:
+        print("Correct usage: script, IP address, port number")
+        sys.exit()
 
-while True: 
+    IP_address = sys.argv[1]
+    Port = int(sys.argv[2])
 
-	# maintains a list of possible input streams 
-	sockets_list = [sys.stdin, server] 
+    # Initialize the socket
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect((IP_address, Port))
 
-	""" There are two possible input situations. Either the 
-	user wants to give manual input to send to other people, 
-	or the server is sending a message to be printed on the 
-	screen. Select returns from sockets_list, the stream that 
-	is reader for input. So for example, if the server wants 
-	to send a message, then the if condition will hold true 
-	below.If the user wants to send a message, the else 
-	condition will evaluate as true"""
-	read_sockets,write_socket, error_socket = select.select(sockets_list,[],[]) 
+    # On Windows, `select` may not work properly with standard input (sys.stdin)
+    # In this case, we use `msvcrt` to check for keyboard input
+    use_msvcrt = os.name == 'nt'
 
-	for socks in read_sockets: 
-		if socks == server: 
-			message = socks.recv(2048) 
-			print (message.decode()) 
-		else: 
-			message = sys.stdin.readline() 
-			server.send(message.encode()) 
-			sys.stdout.write("<You>") 
-			sys.stdout.write(message) 
-			sys.stdout.flush() 
-server.close() 
+    try:
+        while True:
+            sockets_list = [server]
+            if not use_msvcrt:
+                sockets_list.append(sys.stdin)  # Only add sys.stdin if not on Windows
 
+            # Wait for input on server socket or stdin
+            read_sockets, _, _ = select.select(sockets_list, [], [])
+
+            for socks in read_sockets:
+                if socks == server:
+                    # Server has sent a message
+                    message = socks.recv(2048)
+                    if message:
+                        print(message.decode())
+                    else:
+                        print("Connection closed by server.")
+                        sys.exit()
+                else:
+                    # User has typed a message
+                    if use_msvcrt:  # Windows-specific input handling
+                        import msvcrt
+                        if msvcrt.kbhit():
+                            message = input()
+                            if message:
+                                server.send(message.encode())
+                                print(f"<You> {message}")
+                    else:  # Unix-like systems
+                        message = sys.stdin.readline()
+                        if message:
+                            server.send(message.encode())
+                            print(f"<You> {message}", end='')
+
+    except KeyboardInterrupt:
+        print("Disconnected from chat.")
+    finally:
+        server.close()
+
+if __name__ == "__main__":
+    main()
